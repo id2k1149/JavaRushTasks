ba384253bbbd8e6699127a587220b9e98d58de4d
testertt <id2k1149@gmail.com>
2020-12-08 14:45:44 -0500

    ma 0308-12

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0308;
+
+/* 
+Произведение 10 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int result = 1;
+        for (int i = 2; i <= 10; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+
+    }
+}
