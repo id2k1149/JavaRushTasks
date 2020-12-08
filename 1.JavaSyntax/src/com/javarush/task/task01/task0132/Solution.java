0cb530752c72373330bcd6ebb30c9ca907758792
MikeP <id2k1149@gmail.com>
2020-12-08 17:20:40 -0500

    from mb 08/12/20 0134

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0132/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task01.task0132;
+
+/* 
+Сумма цифр трехзначного числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(546));
+    }
+
+    public static int sumDigitsInNumber(int number) {
+        //напишите тут ваш код
+
+        int sum = 0;
+        int hundreds = number / 100;
+        int tens = (number - hundreds * 100) / 10;
+        int numbers = number - hundreds * 100 - tens * 10;
+        sum = hundreds + tens + numbers;
+        return sum;
+
+    }
+}
