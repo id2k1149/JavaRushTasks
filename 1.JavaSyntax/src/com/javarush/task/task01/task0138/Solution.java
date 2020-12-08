fe4cb0f15e7e6c7e93c30196b53350c5c6286188
testertt <id2k1149@gmail.com>
2020-12-07 21:51:30 -0500

    07 dec 2020

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        for (int i=0; i<4; i++)
+            System.out.println("Элли самая красивая");
+    }
+}
