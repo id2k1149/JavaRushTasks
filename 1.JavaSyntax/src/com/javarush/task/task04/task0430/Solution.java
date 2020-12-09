a93470df909bbe22b44ffd293103d097c0c19a53
MikeP <id2k1149@gmail.com>
2020-12-09 17:05:47 -0500

    from mb 09/12/20 0443

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0430/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task04.task0430;
+
+/* 
+От 1 до 10
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int n = 0;
+        while (n < 10) {
+            n ++;
+            System.out.println(n);
+        }
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0431/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0431;
+
+/* 
+От 10 до 1
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int n = 10;
+        while (n > 0) {
+
+            System.out.println(n);
+            n--;
+        }
+
+    }
+}
